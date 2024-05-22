19ab999e72a3bbf76b69956a2ccded81621dc594
Oleshchuk Andrew <aoleshchuk@tdera.ru>
2024-05-22 06:33:49 +0300

    added proxy

--- src/api/index.js
+++ src/api/index.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const fetchData = async (url) => (await axios.get(url)).data;
 
-export const getCountries = () => fetchData('https://5sim.biz/v1/guest/countries');
+export const getCountries = () => fetchData('/api/v1/guest/countries');
 
-export const getPricesByCountryAndProduct = (country, product) => fetchData(`https://5sim.biz/v1/guest/prices?country=${country}&product=${product}`);
+export const getPricesByCountryAndProduct = (country, product) => fetchData(`/api/v1/guest/prices?country=${country}&product=${product}`);
 
-export const getServices = () => fetchData(`https://5sim.biz/v1/guest/products/any/any`);
+export const getServices = () => fetchData(`/api/v1/guest/products/any/any`);
 
 
--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '5sim',
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'https://5sim.biz',
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, ''),
-  //     },
-  //   },
-  // },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://5sim.biz',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 });
