4fbf56245c44963a43ed206e790cb567f658271d
Oleshchuk Andrew <aoleshchuk@tdera.ru>
2024-05-23 02:59:10 +0300

    added load indicator

--- src/Diagram/index.jsx
+++ src/Diagram/index.jsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
+  CircularProgress,
   FormControl,
   InputLabel,
   MenuItem,
@@ -23,7 +24,7 @@ const Diagram = () => {
   const [countriesNames, setCountriesNames] = useState('');
 
   const { data: countries } = useCountries();
-  const { data: services } = useServices();
+  const { data: services, isFetching } = useServices();
 
   useEffect(() => {
     if (!countriesNames && countries) {
@@ -41,65 +42,70 @@ const Diagram = () => {
       <Typography variant="h5" gutterBottom>
         Диаграмма
       </Typography>
-      <Stack gap={1} direction="row" pb={2} flexWrap="wrap">
-        <FormControl sx={{ minWidth: 270 }}>
-          <InputLabel>Сервис</InputLabel>
-          <Select
-            value={service}
-            label="Сервис"
-            onChange={e => setService(e.target.value)}
-            MenuProps={{
-              PaperProps: {
-                style: {
-                  maxHeight: 400,
-                },
-              },
-            }}
-          >
-            {services?.map(service => (
-              <MenuItem key={service} value={service}>
-                {service}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
 
-        <FormControl sx={{ minWidth: 270 }}>
-          <InputLabel>Страна</InputLabel>
-          <Select
-            value={country}
-            onChange={e => setCountry(e.target.value)}
-            label="Страна"
-            MenuProps={{
-              PaperProps: {
-                style: {
-                  maxHeight: 400,
+      {isFetching ? (
+        <CircularProgress size={30} />
+      ) : (
+        <Stack gap={1} direction="row" pb={2} flexWrap="wrap">
+          <FormControl sx={{ minWidth: 270 }}>
+            <InputLabel>Сервис</InputLabel>
+            <Select
+              value={service}
+              label="Сервис"
+              onChange={e => setService(e.target.value)}
+              MenuProps={{
+                PaperProps: {
+                  style: {
+                    maxHeight: 400,
+                  },
                 },
-              },
-            }}
-          >
-            {countries?.map(({ label, value }) => (
-              <MenuItem key={value} value={value}>
-                {label}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+              }}
+            >
+              {services?.map(service => (
+                <MenuItem key={service} value={service}>
+                  {service}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
 
-        <FormControl sx={{ minWidth: 200 }}>
-          <InputLabel>Цена</InputLabel>
-          <Select
-            label="Цена"
-            value={price || ''}
-            onChange={e => setPrice(e.target.value)}
-          >
-            <MenuItem value="10">&lt;10</MenuItem>
-            <MenuItem value="20">&lt;30</MenuItem>
-            <MenuItem value="50">&lt;50</MenuItem>
-            <MenuItem value="100">&lt;100</MenuItem>
-          </Select>
-        </FormControl>
-      </Stack>
+          <FormControl sx={{ minWidth: 270 }}>
+            <InputLabel>Страна</InputLabel>
+            <Select
+              value={country}
+              onChange={e => setCountry(e.target.value)}
+              label="Страна"
+              MenuProps={{
+                PaperProps: {
+                  style: {
+                    maxHeight: 400,
+                  },
+                },
+              }}
+            >
+              {countries?.map(({ label, value }) => (
+                <MenuItem key={value} value={value}>
+                  {label}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+
+          <FormControl sx={{ minWidth: 200 }}>
+            <InputLabel>Цена</InputLabel>
+            <Select
+              label="Цена"
+              value={price || ''}
+              onChange={e => setPrice(e.target.value)}
+            >
+              <MenuItem value="10">&lt;10</MenuItem>
+              <MenuItem value="20">&lt;30</MenuItem>
+              <MenuItem value="50">&lt;50</MenuItem>
+              <MenuItem value="100">&lt;100</MenuItem>
+            </Select>
+          </FormControl>
+        </Stack>
+      )}
 
       {countriesNames && (
         <PriceList
